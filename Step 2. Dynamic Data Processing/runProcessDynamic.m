c7a7b99453090831e47bdbeae340c929a9401ea6
YG Ko <yungunn0072@gmail.com>
2022-04-02 15:21:32 -0700

    added step 2

---
+++ Step 2. Dynamic Data Processing/runProcessDynamic.m
@@ -0,0 +1,9 @@
+load ..\data\P14_DYN_50_P25.mat
+load ..\data\P14model-ocv.mat
+data.temp    = 25; % temperature
+data.script1 = DYNData.script1;
+data.script2 = DYNData.script2;
+data.script3 = DYNData.script3;
+
+model = processDynamic(data,model,1,1);
+save('P14model.mat','model')
